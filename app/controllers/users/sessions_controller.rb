f6c43fd3cb4c1ca8d3b49243a6d3cbefc3dcea34
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-02-28 02:22:01 +0900

    defined_i18n

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 # Users
-class Users < Devise::SessionsController
-  # sessions
-  class SessionsController < Devise::SessionsController
-    def edit; end
+class Users::SessionsController < Devise::SessionsController
+  def edit; end
 
-    def new_guest
-      user = User.guest
-      sign_in user
-      redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-    end
+  def new_guest
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end

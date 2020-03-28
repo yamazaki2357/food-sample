925474e4f49a5546b078587365bead4620f0893c
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-03-28 22:31:09 +0900

    add_image_preview

--- app/javascript/product.js
+++ app/javascript/product.js
@@ -1,12 +1,43 @@
 $(document).on('turbolinks:load', function() {
-               $("#myModal").on("shown.bs.modal", function(event) {
-                 var button = $(event.relatedTarget);
-                 var name = button.data("name");
-                 var number = button.data("number");
-                 var id = button.data("id");
-                 var url = button.data("url");
-                 var modal = $(this);
-                 modal.find(".modal-body").text("品番:" + number + " " + name + "を削除します。");
-                 modal.find("form").attr("action", url);
-               });
-             });
+
+  // 文字色を赤に変える: javascriptが効いているかどうかの確認用
+  $(".card-text").css("color", "red");
+  $(".form-group").css("color", "red");
+
+  $("#myModal").on("shown.bs.modal", function(event) {
+    var button = $(event.relatedTarget);
+    var name = button.data("name");
+    var number = button.data("number");
+    var id = button.data("id");
+    var url = button.data("url");
+    var modal = $(this);
+    modal.find(".modal-body").text("品番:" + number + " " + name + "を削除します。");
+    modal.find("form").attr("action", url);
+  });
+
+
+  $('#img_field').click(function () {
+    $("#file").click();
+  })
+
+
+  $fileField = $('#file')
+
+  $($fileField).on('change', $fileField, function (e) {
+    file = e.target.files[0]
+    reader = new FileReader(),
+    $preview = $("#img_field");
+
+    reader.onload = (function (file) {
+      return function (e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+          src: e.target.result,
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});

29a9cb40b54c8f977fd1196c8a69f15420a94f27
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-04-06 21:52:05 +0900

    modify_image_management

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,7 +1,9 @@
 class CookingsController < ApplicationController
   before_action :set_cooking, only: %i[show edit update destroy]
+  before_action :set_products, only: %i[new create edit update]
   # CSRFトークン検証をスキップする
   skip_before_action :verify_authenticity_token
+
 
   def index
     @cookings = Cooking.all
@@ -19,6 +21,7 @@ class CookingsController < ApplicationController
   end
 
   def update
+    # binding.pry
     begin
       @cooking.update!(cooking_params)
       redirect_to cookings_url, notice: t('msg.update', name: t('cooking', name: @cooking.cooking_name))
@@ -37,9 +40,6 @@ class CookingsController < ApplicationController
     else
       render 'new'
     end
-
-
-
   end
 
   def destroy
@@ -50,10 +50,14 @@ class CookingsController < ApplicationController
   private
 
   def cooking_params
-    params.require(:cooking).permit(:cooking_name, :image, :user_id, :created_at, :updated_at, :category_id, { :product_ids=> [] })
+    params.require(:cooking).permit(:cooking_name, :category_id, :user_id, :image, :created_at, :updated_at, { :product_ids=> [] }, :image)
   end
 
   def set_cooking
     @cooking = Cooking.find(params[:id])
+  end
+
+  def set_products
+    @products = Product.all
   end
 end
--- app/models/cooking.rb
+++ app/models/cooking.rb
@@ -1,6 +1,8 @@
 class Cooking < ApplicationRecord
   has_many :cooking_product_relations, dependent: :destroy
   has_many :products, through: :cooking_product_relations
+  has_one_attached :image
+  belongs_to :category
 
   def product_by?(cooking)
         cooking_product_relations.where(cooking_id: cooking.id).exists?

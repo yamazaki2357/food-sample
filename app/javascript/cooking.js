6c2b6b031318adc915bf7734049e5adc99dab64f
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-05-25 01:28:46 +0900

    modify slideshow

--- app/javascript/cooking.js
+++ app/javascript/cooking.js
@@ -4,6 +4,7 @@ $(document).on('turbolinks:load', function () {
     $("#graydisplay").html($(this).prop("outerHTML"));
     $("#graydisplay").fadeIn(200);
   });
+  
   $("#graydisplay, #graydisplay img").click(function () {
     $("#graydisplay").fadeOut(200);
   });
@@ -17,17 +18,4 @@ $(document).on('turbolinks:load', function () {
     modal.find(".modal-body").text(name + "を削除します。");
     modal.find("form").attr("action", url);
   });
-
-  // $('.box_container').hover(function () { //box_containerにマウスを乗せた時
-  //   var width = $(this).outerWidth() / 2; //box_containerの幅を取って半分にします
-  //   $(this).find('.left').animate(  //class="left"にアニメーションを与えます
-  //     { right: width }, //上記で取ってきた半分のwidth分ずらします
-  //     { queue: false, duration: 2000 });
-  //   $(this).find('.right').animate( //上記の右側の処理です。
-  //     { left: width },
-  //     { queue: false, duration: 2000 });
-  // }, function () {  //マウスアウトの処理でオン時にずらした分を戻します
-  //   $(this).find('.left').animate({ right: 0 }, { queue: false, duration: 800 });
-  //   $(this).find('.right').animate({ left: 0 }, { queue: false, duration: 800 });
-  // });
 });

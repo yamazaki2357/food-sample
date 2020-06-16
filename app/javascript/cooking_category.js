bdb77a908b6e0278ade9eaa6ca523a04360b76b7
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-06-17 07:26:19 +0900

    modify

--- app/javascript/cooking_category.js
+++ app/javascript/cooking_category.js
@@ -4,11 +4,11 @@ $(document).on('turbolinks:load', function () {
   $(".gallery-item1").addClass('addcolor');
 
   var carousel = document.querySelector(".carousel"),
-    figure = carousel.querySelector("figure"),
-    nav = carousel.querySelector("nav"),
-    numImages = figure.childElementCount,
-    theta = (2 * Math.PI) / numImages,
-    currImage = 0;
+      figure = carousel.querySelector("figure"),
+      nav = carousel.querySelector("nav"),
+      numImages = figure.childElementCount,
+      theta = (2 * Math.PI) / numImages,
+      currImage = 0;
   nav.addEventListener("click", onClick, true);
 
   function onClick(e) {
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require('home.js');
 require('product.js');
 require('cooking.js');
 require('cooking_category.js');
-require('home.js');
 require('flash.js');
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

e200c93c12bcaa2d51186a3b71b7b0dc3e555b5a
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-07-07 06:18:06 +0900

    js_invalidation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.assets.initialize_on_precompile = false
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 end

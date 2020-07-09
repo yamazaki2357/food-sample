0375319200c5d3bb6388a4c965d0efedd08477f3
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-07-09 21:56:41 +0900

    modiry migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_104429) do
+ActiveRecord::Schema.define(version: 2020_07_09_121440) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false

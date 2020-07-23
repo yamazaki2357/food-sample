7e8d4510aa727cc0085052cc0833fa94e66da82c
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-07-23 13:18:58 +0900

    modify foodstuff

--- app/javascript/cooking.js
+++ app/javascript/cooking.js
@@ -19,86 +19,86 @@ $(document).on('turbolinks:load', function () {
     modal.find("form").attr("action", url);
   });
 
-  function change_checkbox(_this) {
-    let productClassification = $(_this).val(); /*食材分類ID取得*/
-    $.ajax({
-      type: "GET",
-      url: "/cookings/foodstuff",
-      data: {
-        product_category_id: productClassification,
-      },
-      dataType: "json",
-    })
-      .done(function (data) {
-        for (let t = 1; t < 29; t++) {
-          if (productClassification == t) {
-            $(
-              `#form_check_box_list${productClassification}`
-            ).show(); /* ドロップダウンリストから選択された食材IDに該当するチェックボックスを表示 */
-          } else {
-            $(`#form_check_box_list${t}`).hide(); /* それ以外は非表示*/
-          }
-        }
-        if (
-          $(`#form_check_box_list${productClassification}`).find("label")
-            .length == 0
-        ) {
-          /* チェックボックスの生成。初めて表示するときだけ適用する */
-          for (let i = 0; i < data.length; i++) {
-            var prop = $(`#cooking_product_ids_${data[i]["id"]}`).prop('checked');
-            // 参照用チェックボックスの値に応じてチェックボックスを生成する
-            if (prop) {
-              let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input" checked="checked"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
-              $(`#form_check_box_list${productClassification}`).append(addedElem);
+  if ($("#form_check_box_list1").length) {
+    function change_checkbox(_this) {
+      let productClassification = $(_this).val(); /*食材分類ID取得*/
+      $.ajax({
+        type: "GET",
+        url: "/cookings/foodstuff",
+        data: {
+          product_category_id: productClassification,
+        },
+        dataType: "json",
+      })
+        .done(function (data) {
+          for (let t = 1; t < 29; t++) {
+            if (productClassification == t) {
+              $(
+                `#form_check_box_list${productClassification}`
+              ).show(); /* ドロップダウンリストから選択された食材IDに該当するチェックボックスを表示 */
             } else {
-              let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
-              $(`#form_check_box_list${productClassification}`).append(addedElem);
+              $(`#form_check_box_list${t}`).hide(); /* それ以外は非表示*/
             }
           }
-        }
-      })
-      .fail(function () {
-        console.log("失敗");
+          if (
+            $(`#form_check_box_list${productClassification}`).find("label")
+              .length == 0
+          ) {
+            /* チェックボックスの生成。初めて表示するときだけ適用する */
+            for (let i = 0; i < data.length; i++) {
+              var prop = $(`#cooking_product_ids_${data[i]["id"]}`).prop('checked');
+              // 参照用チェックボックスの値に応じてチェックボックスを生成する
+              if (prop) {
+                let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input" checked="checked"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
+                $(`#form_check_box_list${productClassification}`).append(addedElem);
+              } else {
+                let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
+                $(`#form_check_box_list${productClassification}`).append(addedElem);
+              }
+            }
+          }
+        })
+        .fail(function () {
+          console.log("失敗");
+        });
+    };
+    // チェックボックス生成イベントをページ読み込み時とドロップダウン変更時に発火させる
+    $(document).ready(function () {
+      change_checkbox("#select_pro_category");
+      // 読み込み時チェックの入っているものを配列に追加する
+      $('input[name="cooking[product_ids][]"]:checked').each(function () {
+        productCheck.push($(this).next("label").text());
       });
-  };
-
-  // チェックボックス生成イベントをページ読み込み時とドロップダウン変更時に発火させる
-  $(document).ready(function () {
-    change_checkbox("#select_pro_category");
-    // 読み込み時チェックの入っているものを配列に追加する
-    $('input[name="cooking[product_ids][]"]:checked').each(function () {
-      productCheck.push($(this).next("label").text());
+      $("#span").text(productCheck);
     });
-    $("#span").text(productCheck);
-  });
-  $("#select_pro_category").change(function () {
-    change_checkbox(this);
-  });
+    $("#select_pro_category").change(function () {
+      change_checkbox(this);
+    });
 
-  // チェックを入れた内容を一覧表示
-  let productCheck = [];
-  $(document).on("change", ".checkbox-input", function () {
-    let arrText = $(this).parent("label").text();
-    console.log(arrText);
-    if (productCheck.some((num) => num === arrText)) {
-      // 配列から削除
-      let idx = $.inArray(arrText, productCheck);
-      if (idx >= 0) {
-        productCheck.splice(idx, 1);
+    // チェックを入れた内容を一覧表示
+    let productCheck = [];
+    $(document).on("change", ".checkbox-input", function () {
+      let arrText = $(this).parent("label").text();
+      console.log(arrText);
+      if (productCheck.some((num) => num === arrText)) {
+        // 配列から削除
+        let idx = $.inArray(arrText, productCheck);
+        if (idx >= 0) {
+          productCheck.splice(idx, 1);
+        }
+      } else {
+        // 配列に追加
+        productCheck.push(arrText);
+        productCheck.filter(function (x, i, self) {
+          return self.indexOf(x) === i;
+        });
       }
-    } else {
-      // 配列に追加
-      productCheck.push(arrText);
-      productCheck.filter(function (x, i, self) {
-        return self.indexOf(x) === i;
-      });
-    }
-    $("#span").text(productCheck);
-  });
+      $("#span").text(productCheck);
+    });
 
-  $(document).on("click", "#reset", function () {
-    productCheck = [];
-    $("#span").text(productCheck);
-  });
-
+    $(document).on("click", "#reset", function () {
+      productCheck = [];
+      $("#span").text(productCheck);
+    });
+  }
 });

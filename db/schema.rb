75105c8f9cadc70a5e8bdca1508d46f92b1f0e27
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-07-25 19:03:48 +0900

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_121440) do
+ActiveRecord::Schema.define(version: 2020_07_25_072123) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_07_09_121440) do
     t.string "product_name"
     t.integer "product_number"
     t.integer "page"
+    t.integer "classification"
     t.boolean "discontinued"
     t.string "unit_name"
     t.integer "price"
@@ -92,8 +93,8 @@ ActiveRecord::Schema.define(version: 2020_07_09_121440) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
-    t.index ["email"], name: "index_users_on_email", unique: true, length: 191
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, length: 191
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"

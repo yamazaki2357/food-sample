ebed6c7710fbec5b2325c383c6ef21e7842b621b
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-07-30 21:58:50 +0900

    modify migrate, add responsive

--- db/migrate/20200730132123_change_category_id_to_product_category_id.rb
+++ db/migrate/20200730132123_change_category_id_to_product_category_id.rb
@@ -1,6 +1,6 @@
 class ChangeCategoryIdToProductCategoryId < ActiveRecord::Migration[6.0]
   def change
     rename_column :cookings, :category_id, :product_category_id
-    rename_column :products, :category_id, :product_category_id
+    # rename_column :products, :category_id, :product_category_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_072123) do
+ActiveRecord::Schema.define(version: 2020_07_30_132123) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
@@ -76,11 +76,12 @@ ActiveRecord::Schema.define(version: 2020_07_25_072123) do
     t.text "remark"
     t.integer "unit"
     t.integer "user_id"
+    t.integer "product_category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "product_category_id"
+    t.bigint "category_id"
     t.bigint "cooking_category_id"
-    t.index ["product_category_id"], name: "index_products_on_product_category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["product_name"], name: "index_products_on_product_name", unique: true
   end
 
@@ -100,5 +101,5 @@ ActiveRecord::Schema.define(version: 2020_07_25_072123) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cooking_product_relations", "cookings"
   add_foreign_key "cooking_product_relations", "products"
-  add_foreign_key "products", "product_categories"
+  add_foreign_key "products", "product_categories", column: "category_id"
 end

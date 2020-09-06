6e27c3c8062b8d0be8261d44b9a0605d489cce5d
yamazaki1005 <50128420+yamazaki1005@users.noreply.github.com>
2020-09-06 14:34:42 +0900

    modiry checkbox,modal,image

--- app/javascript/cooking.js
+++ app/javascript/cooking.js
@@ -1,5 +1,22 @@
 $(document).on('turbolinks:load', function () {
 
+  // // $("#MyCookingModal").ready(function (event) {
+  // $("#MyCookingModal").on("show.bs.modal", function (event) {
+  //   var modal = $(this);
+  //   var button = $(event.relatedTarget);
+  //   var name = button.data("name");
+  //   console.log(name);
+  //   console.log(modal);
+  //   console.log(button);
+  //   console.log("モーダルが読み込まれました。");
+  //   // var id = button.data("id");
+  //   // console.log(id);
+  //   // var url = button.data("url");
+  //   // console.log(url);
+  //   modal.find(".modal-body").text(name + "を削除します。");
+  //   // modal.find("form").attr("action", url);
+  // });
+
   $(".imgArea img").click(function () {
     $("#graydisplay").html($(this).prop("outerHTML"));
     $("#graydisplay").fadeIn(200);
@@ -9,98 +26,104 @@ $(document).on('turbolinks:load', function () {
     $("#graydisplay").fadeOut(200);
   });
 
-  $("#MyCookingModal").on("shown.bs.modal", function (event) {
-    let button = $(event.relatedTarget);
-    let name = button.data("name");
-    let id = button.data("id");
-    let url = button.data("url");
-    let modal = $(this);
-    modal.find(".modal-body").text(name + "を削除します。");
-    modal.find("form").attr("action", url);
-  });
-
-  if ($("#form_check_box_list1").length) {
-    function change_checkbox(_this) {
-      let productClassification = $(_this).val(); /*食材分類ID取得*/
-      $.ajax({
-        type: "GET",
-        url: "/cookings/foodstuff",
-        data: {
-          product_category_id: productClassification,
-        },
-        dataType: "json",
-      })
-        .done(function (data) {
-          for (let t = 1; t < 29; t++) {
-            if (productClassification == t) {
-              $(
-                `#form_check_box_list${productClassification}`
-              ).show(); /* ドロップダウンリストから選択された食材IDに該当するチェックボックスを表示 */
+  // ドロップボックスに応じたチェックボックスを生成する
+  function generate_checkbox(productClassification) {
+    // IDを元に選択したドロップボックスに該当するチェックボックスを配列として取得
+    $.ajax({
+      type: "GET",
+      url: "/cookings/foodstuff",
+      data: {
+        product_category_id: productClassification,
+      },
+      dataType: "json",
+    })
+      .done(function (data) {
+        if (
+          $(`#form_check_box_list${productClassification}`).find("label")
+            .length == 0
+        ) {
+          /* チェックボックスの生成。初めて表示するときだけ適用する */
+          for (let i = 0; i < data.length; i++) {
+            var prop = $(`#cooking_product_ids_${data[i]["id"]}`).prop(
+              "checked"
+            );
+            // 参照用チェックボックスの値に応じてチェックボックスを生成する
+            if (prop) {
+              let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input" checked="checked"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
+              $(`#form_check_box_list${productClassification}`).append(
+                addedElem
+              );
             } else {
-              $(`#form_check_box_list${t}`).hide(); /* それ以外は非表示*/
+              let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
+              $(`#form_check_box_list${productClassification}`).append(
+                addedElem
+              );
             }
           }
-          if (
-            $(`#form_check_box_list${productClassification}`).find("label")
-              .length == 0
-          ) {
-            /* チェックボックスの生成。初めて表示するときだけ適用する */
-            for (let i = 0; i < data.length; i++) {
-              var prop = $(`#cooking_product_ids_${data[i]["id"]}`).prop('checked');
-              // 参照用チェックボックスの値に応じてチェックボックスを生成する
-              if (prop) {
-                let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input" checked="checked"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
-                $(`#form_check_box_list${productClassification}`).append(addedElem);
-              } else {
-                let addedElem = `<label for="cooking_product_ids_${data[i]["id"]}"><input type="checkbox" value="${data[i]["id"]}" name="cooking[product_ids][]" id="cooking_product_ids_${data[i]["id"]}" class="checkbox-input"><span class="checkbox-parts">${data[i]["product_name"]}</span></input></label>`;
-                $(`#form_check_box_list${productClassification}`).append(addedElem);
-              }
-            }
-          }
-        })
-        .fail(function () {
-          console.log("失敗");
-        });
-    };
-    // チェックボックス生成イベントをページ読み込み時とドロップダウン変更時に発火させる
-    $(document).ready(function () {
-      change_checkbox("#select_pro_category");
+        }
+      })
+      .fail(function () {
+        console.log("失敗");
+      });
+  };
+
+  $(document).ready(function () {
+    // ドロップダウンがある画面だったらチェックボックス生成等一連の処理をする
+    if ($("#select_pro_category").length > 0) {
+      for (let i = 1; i < 29; i++) {
+        generate_checkbox(i);
+      }
+      let productClassification = $("#select_pro_category").val(); /*食材分類ID取得*/
+      $(`#form_check_box_list${productClassification}`).show();
+
       // 読み込み時チェックの入っているものを配列に追加する
       $('input[name="cooking[product_ids][]"]:checked').each(function () {
         productCheck.push($(this).next("label").text());
       });
-      $("#span").text(productCheck);
-    });
-    $("#select_pro_category").change(function () {
-      change_checkbox(this);
-    });
 
-    // チェックを入れた内容を一覧表示
-    let productCheck = [];
-    $(document).on("change", ".checkbox-input", function () {
-      let arrText = $(this).parent("label").text();
-      console.log(arrText);
-      if (productCheck.some((num) => num === arrText)) {
-        // 配列から削除
-        let idx = $.inArray(arrText, productCheck);
-        if (idx >= 0) {
-          productCheck.splice(idx, 1);
-        }
+      $("#span").text(productCheck);
+    };
+  });
+
+// =================================================================
+  $("#select_pro_category").change(function () {
+    let productClassification = $("#select_pro_category").val(); /*食材分類ID取得*/
+    for (let t = 1; t < 29; t++) {
+      if (productClassification == t) {
+        $(`#form_check_box_list${productClassification}`).show();
+        /* ドロップダウンリストから選択された食材IDに該当するチェックボックスを表示 */
       } else {
-        // 配列に追加
-        productCheck.push(arrText);
-        productCheck.filter(function (x, i, self) {
-          return self.indexOf(x) === i;
-        });
+        $(`#form_check_box_list${t}`).hide(); /* それ以外は非表示*/
       }
-      $("#span").text(productCheck);
-    });
+    }
+  });
+// ================================================================
+  // チェックを入れた内容を一覧表示
+  let productCheck = [];
+  $(document).on("change", ".checkbox-input", function () {
+    let arrText = $(this).parent("label").text();
+    console.log(arrText);
+    if (productCheck.some((num) => num === arrText)) {
+      // 配列から削除
+      let idx = $.inArray(arrText, productCheck);
+      if (idx >= 0) {
+        productCheck.splice(idx, 1);
+      }
+    } else {
+      // 配列に追加
+      productCheck.push(arrText);
+      productCheck.filter(function (x, i, self) {
+        return self.indexOf(x) === i;
+      });
+    }
+    $("#span").text(productCheck);
+  });
 
-    $(document).on("click", "#reset", function () {
-      productCheck = [];
-      $("#span").text(productCheck);
-    });
-  }
+  $(document).on("click", "#reset", function () {
+    productCheck = [];
+    $("#span").text(productCheck);
+  });
+
 
   $(function() {
     $('.directUpload').find("input:file").each(function (i, elem) {
